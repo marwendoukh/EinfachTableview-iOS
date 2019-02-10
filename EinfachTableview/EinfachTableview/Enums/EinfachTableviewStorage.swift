2544f0d1dadcb45e183c9eb864c1f3c907650401
Marwen Doukh <marwen.doukh@gmail.com>
2019-02-10 14:49:17 +0100

    add save data in Realm feature

---
+++ EinfachTableview/EinfachTableview/Enums/EinfachTableviewStorage.swift
@@ -0,0 +1,9 @@
+//
+//  EinfachTableviewStorage.swift
+//  EinfachTableview
+//
+//  Created by Marwen Doukh on 10.02.19.
+//  Copyright © 2019 Marwen Doukh. All rights reserved.
+//
+
+import Foundation
---
+++ EinfachTableview/EinfachTableview/Repositories/RealmDataStorage.swift
@@ -0,0 +1,9 @@
+//
+//  RealmDataStorage.swift
+//  EinfachTableview
+//
+//  Created by Marwen Doukh on 10.02.19.
+//  Copyright © 2019 Marwen Doukh. All rights reserved.
+//
+
+import Foundation
